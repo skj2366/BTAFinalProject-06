960fc7aba14c8ff63ba586b9f36da051697cc94a
Ian <ian@Ianui-MacBookAir.local>
2022-11-02 23:42:48 +0900

    [#15]Docs:Page Routing & add Header comp(routing test)

---
+++ explorer/client/src/pages/TX.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TX = () => {
+  return (
+    <div>TX</div>
+  )
+}
+
+export default TX
---
+++ explorer/client/src/pages/TX_DETAIL.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TX_DETAIL = () => {
+  return (
+    <div>TX_DETAIL</div>
+  )
+}
+
+export default TX_DETAIL
