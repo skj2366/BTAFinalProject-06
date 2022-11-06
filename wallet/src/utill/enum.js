ab93c813507339d7e684cc2cd6622001ee01c4a8
chohee <joy@olulo.io>
2022-11-06 20:33:55 +0900

    issue#30 daemon 노드 연결

--- wallet/src/utill/enum.js
+++ wallet/src/utill/enum.js
@@ -12,5 +12,5 @@ export const NetworkType = {
   MainNetwork: new StacksMainnet(),
   TestNetwork: new StacksTestnet(),
   // url 설정 필요 기본 'http://localhost:3999'
-  customNetwork: new StacksMocknet()
+  customNetwork: new StacksMocknet({url: `http://localhost:3999`})
 }
