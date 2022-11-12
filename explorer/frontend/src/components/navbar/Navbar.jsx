32293cc6f7b3ff98c737b13ef229ee7a6e15a7b1
DongKeun, Kim <skj2366@naver.com>
2022-11-13 03:23:55 +0900

    [#60] feat: add block & block detail page / account nav connect

--- explorer/frontend/src/components/navbar/Navbar.jsx
+++ explorer/frontend/src/components/navbar/Navbar.jsx
@@ -1,47 +1,82 @@
-import React from 'react'
-import {NavLink} from 'react-router-dom'
-import { Button, notification } from 'antd'
-import { InfoOutlined } from '@ant-design/icons'
-import { Select } from 'antd'
-import './Navbar.css'
-import { useDispatch } from 'react-redux'
-import { selectNet } from '../../redux/netSlice'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Button, notification } from 'antd';
+import { InfoOutlined } from '@ant-design/icons';
+import { Select } from 'antd';
+import './Navbar.css';
+import { useDispatch } from 'react-redux';
+import { selectNet } from '../../redux/netSlice';
+import Search from 'antd/lib/transfer/search';
 
 const Navbar = () => {
-    const {Option} = Select
-    const dispatch = useDispatch()
+  const { Option } = Select;
+  const dispatch = useDispatch();
 
-    const openNotification = () => {
-        const args = {
-            message: 'Beta',
-            description: '사이트는 베타 테스트 중입니다. 버그를 발견하면 개발자에게 보고하세요.',
-            duration: 6,
-        }
-        notification.open(args)
-    }
+  // const openNotification = () => {
+  //     const args = {
+  //         message: 'Beta',
+  //         description: '사이트는 베타 테스트 중입니다. 버그를 발견하면 개발자에게 보고하세요.',
+  //         duration: 6,
+  //     };
+  //     notification.open(args);
+  // };
 
   return (
     <nav className='navbar'>
-        <div className='navbar__content wrapper'>
-            <div className='navbar__logo'> <NavLink to= '/'> <span className='hbar__logo'>&#8463;</span>explorer</NavLink></div>
-            <div className='beta'>
-                <Select 
-                    className='navbar__net'
-                    defaultValue='localnet'
-                    style={{width: 150}}
-                    onChange={value => dispatch(selectNet(value)) }
-                    >
-                    <Option value="localnet">localnet</Option>
-                    <Option value="mainnet">Mainnet</Option>
-                    <Option value="testnet">Testnet</Option>
-                </Select>
-
-                <Button type='primary' danger = {true} ghost = {true} size='small' shape='circle' onClick={openNotification} icon = {<InfoOutlined style={{ fontSize: '16px'}} />}></Button>
-            </div>
-            {/* <Switch id='switcher' className='navbar__switch' onChange={changeTheme}/> */}
+      <div className='navbar__content wrapper'>
+        <div className='navbar__logo'>
+          {' '}
+          <NavLink to='/'>
+            {' '}
+            <span className='hbar__logo'>&#8463;</span>explorer
+          </NavLink>
         </div>
-    </nav>
-  )
-}
+        <div className='beta'>
+          <Select
+            className='navbar__net'
+            defaultValue='localnet'
+            style={{ width: 150 }}
+            onChange={(value) => dispatch(selectNet(value))}
+          >
+            <Option value='localnet'>localnet</Option>
+            <Option value='mainnet'>Mainnet</Option>
+            <Option value='testnet'>Testnet</Option>
+          </Select>
 
-export default Navbar
+          <Button type='link' ghost={true} size='small'>
+            <NavLink to={`/`}>Dash Board</NavLink>
+          </Button>
+          <Button type='link' ghost={true} size='small'>
+            <NavLink to={`/transactions`}>Transactions</NavLink>
+          </Button>
+          <Button type='link' ghost={true} size='small'>
+            <NavLink to={`/account`}>Accounts</NavLink>
+          </Button>
+          <Button type='link' ghost={true} size='small'>
+            <NavLink to={`/blocks`}>Blocks</NavLink>
+          </Button>
+
+          <Search
+            placeholder='AccountId or TxId or Block Number'
+            // onChange={(e) => setAcc(e.target.value)}
+            // onSearch={(e) => searchHandle('account', acc)}
+            style={{ width: 300 }}
+          />
+
+          {/* <Button
+            type='primary'
+            danger={true}
+            ghost={true}
+            size='small'
+            shape='circle'
+            onClick={openNotification}
+            icon={<InfoOutlined style={{ fontSize: '16px' }} />}
+          ></Button> */}
+        </div>
+        {/* <Switch id='switcher' className='navbar__switch' onChange={changeTheme}/> */}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- explorer/frontend/src/config.js
+++ explorer/frontend/src/config.js
@@ -1,5 +1,6 @@
-export const LOCALNET_API_URL = "localhost:8080"
-export const MAINNET_API_URL = "mainnet-public.mirrornode.hedera.com"
-export const TESTNET_API_URL = "testnet.mirrornode.hedera.com"
+export const LOCALNET_API_URL = 'localhost:8080';
+export const LOCALNET_API_URL2 = 'localhost:5551';
+export const MAINNET_API_URL = 'mainnet-public.mirrornode.hedera.com';
+export const TESTNET_API_URL = 'testnet.mirrornode.hedera.com';
 
-export const BALANCES = "/api/v1/balances"
+export const BALANCES = '/api/v1/balances';
