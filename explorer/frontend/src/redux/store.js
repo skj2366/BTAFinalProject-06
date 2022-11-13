8f995c474fdc9e8959be0e1ff936e1439ac02d55
dhkim <rlawkdgks11@gmail.com>
2022-11-13 21:47:59 +0900

    fix: 계정 페이지 및 api 미들웨어 추가

--- explorer/frontend/src/redux/store.js
+++ explorer/frontend/src/redux/store.js
@@ -14,7 +14,7 @@ const rootReducer = combineReducers({
 
 export const store = configureStore({
     reducer: rootReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(hbarApi.middleware, hederaApi.middleware)
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(hbarApi.middleware, hederaApi.middleware, serverApi.middleware)
 });
 
 setupListeners(store.dispatch)
