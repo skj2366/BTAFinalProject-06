6aee5ca341fd51dc2f08c3482901c05b8cad59d2
dhkim <rlawkdgks11@gmail.com>
2022-11-13 16:36:50 +0900

    fix: block 관련 타임스탬프 변환 수정

--- explorer/frontend/src/utils/GetTime.js
+++ explorer/frontend/src/utils/GetTime.js
@@ -1,7 +1,29 @@
 export default function getTime(time) {
     console.log(time)
-    if(time) {
-        let _time = time.split('.')
-        return new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(_time[0]*1000)
-    } else {return ''}
+    if (time) {
+        let _time;
+        if (time.toString().includes('.')) {
+            _time = time.split('.')
+            return new Intl
+                .DateTimeFormat('en-US', {
+                    year: 'numeric',
+                    month: '2-digit',
+                    day: '2-digit',
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    second: '2-digit'
+                })
+                .format(_time[0] * 1000)
+        } else {
+            var ts = new Date(Number(time) * 1000);
+            const date = ts.toLocaleString("en-GB", {
+                dateStyle: 'short',
+                timeStyle: 'medium'
+            });
+            console.log(date);
+            return date;
+        }
+    } else {
+        return ''
+    }
 }
